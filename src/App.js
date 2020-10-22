bf29de5720217acb059319cd610d0d53f5326227
Leonid <valeriy92prog@gmail.com>
2020-10-22 15:21:52 +0300

    data init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Grid} from "@material-ui/core";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let state = {
+        mainData: [],
+        mainData2: []
+    };
+
+    const setData = () => {
+        fetch('https://storage.googleapis.com/aller-structure-task/test_data.json')
+            .then(response => response.json())
+            .then((jsonData) => {
+                state.mainData = jsonData[0];
+            })
+            .catch((error) => {
+                console.error(error)
+            });
+    };
+
+    const getArticles = () => {
+        console.log(state.mainData);
+
+        state.mainData2 = state.mainData.map((article) => {
+            console.log(article);
+            return article;
+        });
+        console.log(state.mainData2);
+    };
+
+    return (
+        <div className="App">
+            {setData()}
+            <header className="App-header">
+
+            </header>
+            <main>
+                <div>
+                    <button onClick={getArticles}>Get Article</button>
+                    <ul>
+                        {
+                            state.mainData2.map((article, index) => {
+                                console.log(article);
+                                return <li key={index}>{article.type}</li>
+                            })
+                        }
+                    </ul>
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
